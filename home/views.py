1965e6e9f585dcd507ae762ff54713967f599f11
Jayasree7917 <158481485+Jayasree7917@users.noreply.github.com>
2024-03-14 13:34:15 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -4,7 +4,7 @@ from home.models import Contact
 
 # Create your views here.
 def index(request):
-   return render(request, 'index.html')
+   return render(request,'index.html')
 
 def contact(request):
    if request.method == "POST":
@@ -14,4 +14,4 @@ def contact(request):
       message = request.POST.get('message')
       contact = Contact(name=name, email=email, phone=phone, message=message, date=datetime.today())
       contact.save()
-   return render(request, 'index.html')
+   return render(request,'index.html')
